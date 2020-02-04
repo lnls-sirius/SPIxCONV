a83fd95af6cc7c98f28c99106d85696e776c09cd
Rafael Ito <ito.rafael@gmail.com>
2020-02-04 15:18:11 -0300

    uncommented board_address argument

--- software/scripts/spixconv_unix_socket.py
+++ software/scripts/spixconv_unix_socket.py
@@ -411,7 +411,7 @@ if __name__ == '__main__':
     logger = logging.getLogger()
 
     parser = argparse.ArgumentParser(description='SPIxCONV Socket Binding', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    #parser.add_argument('board_address', type=int, help='Board address.')
+    parser.add_argument('board_address', type=int, help='Board address.')
     parser.add_argument('--tcp', help='Use a TCP socket instead of an UNIX.', action='store_true')
     parser.add_argument('--port', '-p', dest='port', type=int, help='TCP server port.', default=5005)
     parser.add_argument('--unix-address', dest='unix_socket_address', help='UNIX socket address.', default='/tmp/socket_spixconv')
