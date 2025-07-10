1684ce245f1cb8dbc0af59abd89a71e74d84e994
Mauro Luiz Brandao Junior <mrbrjr@gmail.com>
2025-07-10 17:06:57 -0300

    Change loop definition
    This updates the loop definition to match an unmerged modification
    introduced in the logging branch. This ensures consistency between
    the two branches.

--- software/scripts/spixconv_unix_socket.py
+++ software/scripts/spixconv_unix_socket.py
@@ -565,7 +565,8 @@ if __name__ == '__main__':
                 raise
     #----------------------------
     # identify board address:
-    for addr in range(255):
+    #for addr in range(255):
+    for addr in list(range(10, -1, -1)):
         if(flash.ID_read(addr) == 4):
             board_address = addr
             break
